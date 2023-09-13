6db1c94585849ce95d56c2bdc56288a3a8c00cf3
ikhwanizao <ikhwanm32@gmail.com>
2023-09-13 15:13:44 +0800

    Add project title
    Add project title and fix responsive issue

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,8 @@ let projectsHTML = "";
 projects.forEach((project, index) => {
   if (index % 2 === 0) {
     projectsHTML += `<li id="${project.id}">
+    <h3 class="project-title">${project.title}</h3>
+    <div class="project-content">
         <div class="project-screenshot">
         <a
             href="${project.githubRepo}"
@@ -25,9 +27,12 @@ projects.forEach((project, index) => {
             >
         </div>
         </div>
+    </div>
   </li>`;
   } else {
     projectsHTML += `<li id="${project.id}">
+    <h3 class="project-title">${project.title}</h3>
+    <div class="project-content">
     <div class="project-description">
         ${project.description}
         <div class="github-repo">
@@ -48,6 +53,7 @@ projects.forEach((project, index) => {
             alt="Screenshot of ${project.title}"
         /></a>
         </div>
+    </div>
   </li>`;
   }
 });
